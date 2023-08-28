8397ac0efe3f16e97b5e9bb8c8f71712377de962
0xshoegazer <0xshoegazer@gmail.com>
2023-08-28 12:01:59 -0400

    new hash and factory

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
-export const FACTORY_ADDRESS = '0x38015D05f4fEC8AFe15D7cc0386a126574e8077B'
+export const FACTORY_ADDRESS = '0x3aA90F4b3581a38e98D30246Ee3c12865EdE08A5'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+// export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
 // TODO: This is what I got from parsing the code but may be wrong. Above is Uni's V3 one to try
-// export const POOL_INIT_CODE_HASH = '0xbb146dec9ccf1c9c5c42c6e0656c2ab950ecfc0c6b935864e63ac10b9ce008a0' // New one after updating fees myself
+// With new init fee added
+export const POOL_INIT_CODE_HASH = '0x5892de6063b5d6cffe15c4e490934b0ddcaf8408ce5f76e57f3f3fb07de67322'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
