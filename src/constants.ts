326b1753ecd3ef95de1a002002e2f13a6b7a5bae
0xshoegazer <0xshoegazer@gmail.com>
2023-09-01 19:41:39 -0400

    test

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const FACTORY_ADDRESS = '0x38015D05f4fEC8AFe15D7cc0386a126574e8077B'
+export const FACTORY_ADDRESS = '0xEf5793EDcCe5Aa0bbD416A22A6589e534934eA1F'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+export const POOL_INIT_CODE_HASH = '0x7ed774f06f996c7ae326a8ef35e788a72bf0a49adb52e5f1c7067a8b9df041aa'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
